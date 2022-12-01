8378d34481847b4a3e60d907c9b403be62a20b25
Jesse <jesse@rmrz.io>
2022-11-30 22:20:48 -0800

    First commit.

---
+++ qr_generator.py
@@ -0,0 +1,32 @@
+import qrcode
+
+
+def qr_generator():
+    qr = qrcode.QRCode(
+        version=3,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=1
+    )
+
+    qr.add_data(ask_user('Enter a valid URL to create a QR code: '))
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="blue", back_color="white")
+    type(img)
+    img.save(ask_user('Save image as: '))
+
+
+def ask_user(question):
+    ans = input(question)
+    return ans
+
+
+def main():
+    qr_generator()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
